5792de812e114c7d606d618efd13dbc80d8c9b18
Ian McCausland <imccausl@gmail.com>
2017-08-03 13:08:24 -0400

    Renamed modules to components

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './modules/App.jsx';
+import App from './components/App.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();

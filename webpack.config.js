6925d247df814e5969cda973e90773c9fc2a8255
Ian McCausland <imccausl@gmail.com>
2017-08-02 20:29:44 -0400

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    app: 'app.js',
+    vendor: ['react', 'react-dom'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist/assets'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    }),
+  ],
+};

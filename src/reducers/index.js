d477dfc0b62f46c807ca9d32be901432ab33f28c
Ian McCausland <imccausl@gmail.com>
2017-08-03 13:15:03 -0400

    Add rootReducer and populate EmployeesReducer with dummy data

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import EmployeesReducer from './reducer_employees';
+
+const rootReducer = combineReducers({
+  employees: EmployeesReducer,
+});
+
+export default rootReducer;
---
+++ src/reducers/reducer_employees.js
@@ -0,0 +1,11 @@
+const EmployeesModel = () => {
+  return [
+    { name: 'Ian McCausland', hours: 40 },
+    { name: 'Kathleen James', hours: 40 },
+    { name: 'Khavy Tran', hours: 35 },
+    { name: 'John Reyes', hours: 30 },
+    { name: 'Kyle Lee', hours: 24 },
+  ]
+};
+
+export default EmployeesModel;

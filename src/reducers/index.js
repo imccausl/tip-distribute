220a87ac611eca6b570ccdc4e83b8f24ca1ef184
Ian McCausland <imccausl@gmail.com>
2017-08-04 10:15:03 -0400

    Add show modal to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import EmployeesReducer from './reducer_employees';
+import ActiveEmployee from './reducer_active_employee';
+import ShowModal from './reducer_show_modal';
 
 const rootReducer = combineReducers({
   employees: EmployeesReducer,
+  activeEmployee: ActiveEmployee,
+  showModal: ShowModal,
 });
 
 export default rootReducer;

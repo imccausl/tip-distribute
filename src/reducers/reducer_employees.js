342927748c528fa6d1a543597be717f9198c2d69
Ian McCausland <imccausl@gmail.com>
2017-08-05 01:25:19 -0400

    Add mmore dummy data to tipOuts data for testing

--- src/reducers/reducer_employees.js
+++ src/reducers/reducer_employees.js
@@ -1,17 +1,57 @@
-const EmployeesModel = () => {
-  return [
-    {
-      weekEnding: 'July 20',
-      totalCash: 228,
-      employees: [
-        { name: 'Ian McCausland', hours: 40 },
-        { name: 'Kathleen James', hours: 40 },
-        { name: 'Khavy Tran', hours: 35 },
-        { name: 'John Reyes', hours: 30 },
-        { name: 'Kyle Lee', hours: 24 },
-      ],
-    },
-  ];
-};
+const EmployeesModel = () => [
+  {
+    weekEnding: 'July 20',
+    totalCash: 228,
+    employees: [
+      { name: 'Ian McCausland', hours: 40 },
+      { name: 'Kathleen James', hours: 40 },
+      { name: 'Khavy Tran', hours: 35 },
+      { name: 'John Reyes', hours: 30 },
+      { name: 'Kyle Lee', hours: 24 },
+    ],
+  },
+  {
+    weekEnding: 'July 27',
+    totalCash: 200,
+    employees: [
+      { name: 'Ian McCausland', hours: 40 },
+      { name: 'Kathleen James', hours: 40 },
+      { name: 'Khavy Tran', hours: 35 },
+      { name: 'John Reyes', hours: 30 },
+      { name: 'Kyle Lee', hours: 24 },
+      { name: 'Johnny Lee Miller', hours: 24 },
+      { name: 'Drishti Thakar', hours: 10 },
+      { name: 'Paul Sheffield', hours: 28 },
+    ],
+  },
+  {
+    weekEnding: 'August 2',
+    totalCash: 189,
+    employees: [
+      { name: 'Ian McCausland', hours: 40 },
+      { name: 'Kathleen James', hours: 40 },
+      { name: 'Khavy Tran', hours: 35 },
+      { name: 'John Reyes', hours: 30 },
+      { name: 'Kyle Lee', hours: 24 },
+      { name: 'Johnny Lee Miller', hours: 24 },
+      { name: 'Drishti Thakar', hours: 10 },
+      { name: 'Paul Sheffield', hours: 28 },
+    ],
+  },
+  {
+    weekEnding: 'August 9',
+    totalCash: 205,
+    employees: [
+      { name: 'Ian McCausland', hours: 40 },
+      { name: 'Kathleen James', hours: 40 },
+      { name: 'Khavy Tran', hours: 35 },
+      { name: 'John Reyes', hours: 30 },
+      { name: 'Kyle Lee', hours: 24 },
+      { name: 'Johnny Lee Miller', hours: 24 },
+      { name: 'Drishti Thakar', hours: 10 },
+      { name: 'Paul Sheffield', hours: 28 },
+    ],
+  },
+];
 
 export default EmployeesModel;

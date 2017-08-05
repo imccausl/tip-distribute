bdcd0df13598499690be3b29ce3f9a83005d1088
Ian McCausland <imccausl@gmail.com>
2017-08-05 08:17:40 -0400

    Change name of menu item and add divider

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -5,6 +5,7 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import { showDrawer } from '../actions/drawerActions';
 
@@ -14,7 +15,8 @@ const MainMenu = () => (
         targetOrigin={{horizontal: 'right', vertical: 'top'}}
         anchorOrigin={{horizontal: 'right', vertical: 'top'}}
       >
-        <MenuItem primaryText="MenuItem" />
+        <MenuItem primaryText="New Tipout..." />
+        <Divider />
         <MenuItem primaryText="MenuItem2" />
         <MenuItem primaryText="MenuItem3" />
       </IconMenu>

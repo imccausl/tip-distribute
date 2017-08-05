890de083494309b71fb78d84c6f619ec25492b8e
Ian McCausland <imccausl@gmail.com>
2017-08-05 01:24:54 -0400

    Add ddrawer state to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import TipOutsReducer from './reducer_employees';
 import ActiveEmployee from './reducer_active_employee';
 import ActiveTipOut from './reducer_active_tipout';
 import ShowModal from './reducer_show_modal';
+import ShowDrawer from './reducer_show_drawer';
 
 const rootReducer = combineReducers({
   tipOuts: TipOutsReducer,
   activeTipOut: ActiveTipOut,
   activeEmployee: ActiveEmployee,
   showModal: ShowModal,
+  showDrawer: ShowDrawer,
 });
 
 export default rootReducer;

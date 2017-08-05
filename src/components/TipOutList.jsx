a8a7966f54866c4bb0a3b5e348cdc877d50078f3
Ian McCausland <imccausl@gmail.com>
2017-08-05 01:50:24 -0400

    Remove imports that are no longer needed

--- src/components/TipOutList.jsx
+++ src/components/TipOutList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { List } from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
 import { bindActionCreators } from 'redux';
 import TipOutListItem from './TipOutListItem.jsx';
 import selectTipOut from '../actions/tipOutActions';

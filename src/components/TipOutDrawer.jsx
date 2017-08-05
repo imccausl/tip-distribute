6a0493c2d7e21132d87d3904ec49e12b7ea602b1
Ian McCausland <imccausl@gmail.com>
2017-08-05 17:50:10 -0400

    Add global menu to tip out drawer

--- src/components/TipOutDrawer.jsx
+++ src/components/TipOutDrawer.jsx
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Drawer from 'material-ui/Drawer';
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';  
+import IconButton from 'material-ui/IconButton';
+import Divider from 'material-ui/Divider';
 import TipOutList from './TipOutList.jsx';
 
 class TipOutDrawer extends Component {
   constructor(props) {
     super(props);
+  }
+
+  tipOutsMenu() {
+    return (
+      <IconMenu
+        iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+        anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+        targetOrigin={{horizontal: 'left', vertical: 'top'}}
+      >
+        <MenuItem primaryText="New Tip Out..." />
+        <Divider />
+        <MenuItem primaryText="Combine Tip Outs..." />
+        <MenuItem primaryText="Delete Tip Outs..." />
+      </IconMenu>
+    );
   }
 
   render() {
@@ -16,7 +36,10 @@ class TipOutDrawer extends Component {
           open={this.props.drawerOpen}
         >
           <Toolbar>
-            <ToolbarTitle text="Previous Tipouts" />
+            <ToolbarTitle text="Tip Out History" />
+            <ToolbarGroup>
+              {this.tipOutsMenu()}
+            </ToolbarGroup>
           </Toolbar>
           <TipOutList />
         </Drawer>

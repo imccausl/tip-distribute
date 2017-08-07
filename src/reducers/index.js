6379845c272992e2ae8257e670072ec76af926b6
Ian McCausland <imccausl@gmail.com>
2017-08-07 17:56:34 -0400

    Remove unecessary reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,18 +5,14 @@ import ActiveTipOut from './reducer_active_tipout';
 import ShowModal from './reducer_show_modal';
 import ShowDrawer from './reducer_show_drawer';
 import ShowTipOutDialog from './reducer_show_new_tip_out_dialog';
-import ToggleAddPeopleDialog from './reducer_toggle_add_people_dialog';
-import AddPersonToCurrentTipOut from './reducer_add_person_to_tip_out';
 
 const rootReducer = combineReducers({
   tipOuts: TipOutsReducer,
   activeTipOut: ActiveTipOut,
   activeEmployee: ActiveEmployee,
-  addPersonToTipOut: AddPersonToCurrentTipOut,
   showModal: ShowModal,
   showDrawer: ShowDrawer,
   showTipOutDialog: ShowTipOutDialog,
-  toggleAddPeopleDialog: ToggleAddPeopleDialog,
 });
 
 export default rootReducer;

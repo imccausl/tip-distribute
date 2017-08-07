f110d40194e61fe64a05119e65b967141a6e903f
Ian McCausland <imccausl@gmail.com>
2017-08-07 13:32:00 -0400

    Remove reducer for adding tip outs -- affecting wrong piece of state

--- src/reducers/reducer_active_week.js
+++ src/reducers/reducer_active_week.js
@@ -1,7 +1,17 @@
 function activeWeek(state = null, action) {
+  console.log(state, action);
   switch (action.type) {
     case 'WEEK_SELECTED':
       return action.payload;
+    case 'ADD_NEW_TIP_OUT':
+      return [
+        ...state.tipOuts,
+        {
+          weekEnding: action.payload.weekEnding,
+          totalCash: action.payload.totalCash,
+          employees: action.payload.employees,
+        },
+      ];
     default:
       return state;
   }

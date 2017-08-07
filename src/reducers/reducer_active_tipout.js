3997466b87e6a0c5e8aec3bf2f93d97aaf0e5414
Ian McCausland <imccausl@gmail.com>
2017-08-07 17:57:30 -0400

    Move add new person reducer/action to active tipout

--- src/reducers/reducer_active_tipout.js
+++ src/reducers/reducer_active_tipout.js
@@ -1,7 +1,21 @@
 function activeTipOut(state = null, action) {
+  console.log("ActiveTipOut:", state, action);  
   switch (action.type) {
     case 'TIPOUT_SELECTED':
       return action.payload;
+    case 'ADD_NEW_PERSON_TO_CURRENT_TIP_OUT':
+      console.log("state.employees", ...state.employees);
+      return {
+        weekEnding: state.weekEnding,
+        totalCash: state.totalCash,
+        employees: [
+          ...state.employees,
+          {
+            id: action.payload.id,
+            name: action.payload.name,
+            hours: action.payload.hours,
+          },
+        ] };
     default:
       return state;
   }

c63888e806d1518577fbd40bdbdbc0ac4968f328
Ian McCausland <imccausl@gmail.com>
2017-08-07 13:27:48 -0400

    Fix addNewTipOut action to populate with default empty data set

---
+++ src/actions/addNewTipOut.js
@@ -0,0 +1,11 @@
+export default function addNewTipOut(tipOuts) {
+  console.log("in action:", tipOuts);
+  return {
+    type: 'ADD_NEW_TIP_OUT',
+    payload: {
+      weekEnding: tipOuts.weekEnding,
+      totalCash: tipOuts.totalCash,
+      employees: [{ id: 0, name: '', hours: 0 }],
+    },
+  };
+}

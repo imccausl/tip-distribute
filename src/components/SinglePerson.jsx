e97b2989e4deb0fd1a048266573c29928c80d9d1
Ian McCausland <imccausl@gmail.com>
2017-08-07 20:30:25 -0400

    Add makeNewId function to component

--- src/components/SinglePerson.jsx
+++ src/components/SinglePerson.jsx
@@ -7,16 +7,6 @@ import ContentAdd from 'material-ui/svg-icons/content/add-circle-outline';
 import ContentRemove from 'material-ui/svg-icons/content/remove-circle-outline';
 
 class SinglePerson extends Component {
-
-  makeNewId() {
-      const tempId = new Date();
-      const parsedDate = Date.parse(tempId);
-
-      // prevent unique key errors if button is clicked more than once a second
-      const randomizer = Math.floor(Math.random() * parsedDate);
-      return parsedDate + randomizer;
-    }
-
   render() {
     const style = {
       padding: '0 10px',

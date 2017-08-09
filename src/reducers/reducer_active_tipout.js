df3130d5850403b118444cfc5a7b022a292b5c1e
Ian McCausland <imccausl@gmail.com>
2017-08-09 13:41:52 -0400

    Move add person action to different reducer

--- src/reducers/reducer_active_tipout.js
+++ src/reducers/reducer_active_tipout.js
@@ -3,21 +3,6 @@ function activeTipOut(state = null, action) {
   switch (action.type) {
     case 'TIPOUT_SELECTED':
       return action.payload;
-    case 'ADD_NEW_PERSON_TO_CURRENT_TIP_OUT':
-      return Object.assign({}, state, {
-        index: state.index,
-        tipOut: {
-          weekEnding: state.tipOut.weekEnding,
-          totalCash: state.tipOut.totalCash,
-          employees: [...state.tipOut.employees,
-            {
-              id: action.payload.id,
-              name: action.payload.name,
-              hours: action.payload.hours,
-            },
-          ],
-        }
-      });
     default:
       return state;
   }

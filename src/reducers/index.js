7b0419790c12895f3077c78332e3b202c459bb03
Ian McCausland <imccausl@gmail.com>
2017-08-09 13:38:08 -0400

    Add active employees data to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,17 @@
 import { combineReducers } from 'redux';
 import TipOutsReducer from './reducer_employees';
-import ActiveEmployee from './reducer_active_employee';
+import ActivePerson from './reducer_active_employee';
 import ActiveTipOut from './reducer_active_tipout';
 import ShowModal from './reducer_show_modal';
 import ShowDrawer from './reducer_show_drawer';
 import ShowTipOutDialog from './reducer_show_new_tip_out_dialog';
+import ActivePeople from './reducer_active_employees';
 
 const rootReducer = combineReducers({
   tipOuts: TipOutsReducer,
   activeTipOut: ActiveTipOut,
-  activeEmployee: ActiveEmployee,
+  activePeople: ActivePeople,
+  activePerson: ActivePerson,
   showModal: ShowModal,
   showDrawer: ShowDrawer,
   showTipOutDialog: ShowTipOutDialog,

55472b39641cf2b9dbc38e3628ea179476f74115
Ian McCausland <imccausl@gmail.com>
2017-08-10 16:29:32 -0400

    Make longer, safer, GUID compliant ids

--- src/helpers/makeNewId.js
+++ src/helpers/makeNewId.js
@@ -1,8 +1,21 @@
-export default function makeNewId() {
-  const tempId = new Date();
-  const parsedDate = Date.parse(tempId);
+export default function makeUUID() {
+  let d = new Date().getTime();
+  let u = '';
+  let i = 0;
 
-  // prevent unique key errors if button is clicked more than once a second
-  const randomizer = Math.floor(Math.random() * parsedDate);
-  return parsedDate + randomizer;
+  const m = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now();
+  }
+
+  while (i++ < 36) {
+    const c = m[i - 1];
+    const r = ((d + Math.random()) * 16) % 16 | 0;
+    const v = (c === 'x') ? r : ((r & 0x3) | 0x8);
+
+    u += (c === '-' || c === '4') ? c : v.toString(16);
+  }
+
+  return u;
 }

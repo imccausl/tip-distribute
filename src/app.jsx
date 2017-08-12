6f2c77b0d434141e7fb3af0a5e050aac0a8d0b06
Ian McCausland <imccausl@gmail.com>
2017-08-12 10:19:46 -0400

    Moved provier to app root to see what happens

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 
 injectTapEventPlugin();
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // debugger
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root'));

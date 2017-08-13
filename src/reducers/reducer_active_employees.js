6c9e5ae7d94e1fdfae49815e893ea4dd648045e0
Ian McCausland <imccausl@gmail.com>
2017-08-13 02:08:25 -0400

    Added action so that current tip out and entire data tree are updated simultaneously from one function call

--- src/reducers/reducer_active_employees.js
+++ src/reducers/reducer_active_employees.js
@@ -1,15 +1,11 @@
 export default function activeEmployees(state = null, action) {
+  console.log("REDUCERS ACTIVATED!", "ActionType:", action.type, "THE PAYLOAD", action.payload, "prevState:", state);
   switch (action.type) {
     case 'EMPLOYEES_SELECTED':
       return action.payload;
     case 'ADD_NEW_PERSON_TO_CURRENT_TIP_OUT':
       return [
-        ...state,
-        {
-          id: action.payload.id,
-          name: action.payload.name,
-          hours: action.payload.hours,
-        },
+        ...action.payload,
       ];
     default:
       return state;

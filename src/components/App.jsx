dc5c0c3fc71cce66134276cf9bbb0609eb63a6c3
Ian McCausland <imccausl@gmail.com>
2017-08-13 01:59:24 -0400

    Moved redux provider to app root

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import EmployeeList from './EmployeeList.jsx';
-import rootReducer from '../reducers/index';
 import AppBar from './AppBar.jsx';
 import TipOutDrawer from './TipOutDrawer.jsx';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // debugger
-);
-
 const App = () => (
-  <Provider store={store}>
-    <div>
+  <div>
     <MuiThemeProvider>
       <TipOutDrawer />
     </MuiThemeProvider>
@@ -24,8 +15,7 @@ const App = () => (
     <MuiThemeProvider>
       <EmployeeList />
     </MuiThemeProvider>
-    </div>
-  </Provider>
+  </div>
 );
 
 export default App;

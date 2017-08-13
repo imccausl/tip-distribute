25295967866e16ef701170dfb1373d3f3868f434
Ian McCausland <imccausl@gmail.com>
2017-08-13 02:06:48 -0400

    Simplified root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,15 @@
 import { combineReducers } from 'redux';
-import TipOutsReducer from './reducer_employees';
-import ActivePerson from './reducer_active_employee';
-import ActiveTipOut from './reducer_active_tipout';
-import ShowModal from './reducer_show_modal';
+import DataTree from './DataTree';
+import CurrentTipOut from './CurrentTipOut';
+import ModalAction from './reducer_show_modal';
 import ShowDrawer from './reducer_show_drawer';
 import ActivePeople from './reducer_active_employees';
 
 const rootReducer = combineReducers({
-  tipOuts: TipOutsReducer,
-  activeTipOut: ActiveTipOut,
+  dataTree: DataTree,
+  currentTipOut: CurrentTipOut,
   activePeople: ActivePeople,
-  activePerson: ActivePerson,
-  showModal: ShowModal,
+  modalAction: ModalAction,
   showDrawer: ShowDrawer,
 });
 

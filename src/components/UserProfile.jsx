10e2539315cccbdd686403f0072064bb35d2b66e
Ian McCausland <imccausl@gmail.com>
2017-08-17 14:12:13 -0400

    Add user profile edit view

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Card } from 'material-ui/Card';
+
+class UserProfile extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default UserProfile;

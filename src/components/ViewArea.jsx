bb4a6b04d55dcf6bd3a616834e1d58887b1c86d6
Ian McCausland <imccausl@gmail.com>
2017-08-17 14:11:51 -0400

    Begin logic for switching views in the center of app

--- src/components/ViewArea.jsx
+++ src/components/ViewArea.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import EmployeeList from './EmployeeList.jsx';
 
 class ViewArea extends Component {
   render() {
+    const viewArea = () => {
+      switch (this.props.view.type) {
+        case 'EMPLOYEE_LIST':
+          return <EmployeeList />;
+        case 'USER_PROFILE':
+        case 'DISTRIBUTE_TIPS':
+        default:
+          return null;
+      }
+    }
     return (
-      <div style={{margin: '110px 0', zIndex: '0'}}>
-        <EmployeeList />
+      <div style={{ margin: '110px 0', zIndex: '0' }}>
+        {viewArea()}
       </div>
     );
   }

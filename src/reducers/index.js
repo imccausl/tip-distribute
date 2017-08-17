835c28690f19d61194aee4b6f1c3d50b1cee9c22
Ian McCausland <imccausl@gmail.com>
2017-08-17 14:18:05 -0400

    Add reducer for changing state of active view

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,7 @@ import CurrentPerson from './CurrentPerson';
 import ModalAction from './reducer_show_modal';
 import ShowDrawer from './reducer_show_drawer';
 import ActivePeople from './reducer_active_employees';
+import ActiveView from './ActiveView';
 
 const rootReducer = combineReducers({
   firebase: firebaseStateReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   currentTipOut: CurrentTipOut,
   currentPerson: CurrentPerson,
   activePeople: ActivePeople,
+  activeView: ActiveView,
   modalAction: ModalAction,
   showDrawer: ShowDrawer,
 });

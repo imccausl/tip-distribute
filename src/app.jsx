dc693cd6e6e17c779122a97ff173ad9763129f4d
Ian McCausland <imccausl@gmail.com>
2017-08-17 06:39:55 -0400

    Wire up firebasee to redux store

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { reactReduxFirebase } from 'react-redux-firebase';
+import App from './components/App.jsx';
 import rootReducer from './reducers/index';
+import firebaseConfig from './config/firebaseConfig';
 
 injectTapEventPlugin();
-const store = createStore(
+
+const config = {
+  userProfile: 'users',
+  enableLogging: false,
+};
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebaseConfig, config),
+)(createStore);
+
+const store = createStoreWithFirebase(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // debugger
 );

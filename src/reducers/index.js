3d8c7b606ff4420b9f9a71aa7eb9c16fce8f3310
Ian McCausland <imccausl@gmail.com>
2017-08-18 16:16:07 -0400

    Add new and renamed reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,19 @@
 import { combineReducers } from 'redux';
 import { firebaseStateReducer } from 'react-redux-firebase';
-import DataTree from './DataTree';
+import AllTipOuts from './TipOuts';
 import CurrentTipOut from './CurrentTipOut';
 import CurrentPerson from './CurrentPerson';
 import ModalAction from './reducer_show_modal';
 import ShowDrawer from './reducer_show_drawer';
-import ActivePeople from './reducer_active_employees';
+import Tips from './Tips';
 import ActiveView from './ActiveView';
 
 const rootReducer = combineReducers({
   firebase: firebaseStateReducer,
-  dataTree: DataTree,
+  tipOuts: AllTipOuts,
+  tips: Tips,
   currentTipOut: CurrentTipOut,
   currentPerson: CurrentPerson,
-  activePeople: ActivePeople,
   activeView: ActiveView,
   modalAction: ModalAction,
   showDrawer: ShowDrawer,

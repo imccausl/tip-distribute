2a4dbd133236989f6bd8620de844de77ca769460
Ian McCausland <imccausl@gmail.com>
2017-08-20 01:45:53 -0400

    Add auth decorator to AppMain

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { firebaseConnect } from 'react-redux-firebase';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -6,22 +8,26 @@ import ViewArea from './ViewArea.jsx';
 import AppBar from './AppBar.jsx';
 import BottomBar from './BottomBar.jsx';
 import TipOutDrawer from './TipOutDrawer.jsx';
+import UserIsAuthenticated from './UserIsAuthenticated.jsx';
 
-const AppMain = () => (
-  <div style={{ position: 'relative', overflow: 'hidden' }}>
-    <MuiThemeProvider>
-      <TipOutDrawer />
-    </MuiThemeProvider>
-    <MuiThemeProvider>
-      <AppBar />
-    </MuiThemeProvider>
-    <MuiThemeProvider>
-      <ViewArea />
-    </MuiThemeProvider>
-    <MuiThemeProvider>
-      <BottomBar />
-    </MuiThemeProvider>
-  </div>
-);
+@UserIsAuthenticated
+class AppMain extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <MuiThemeProvider>
+          <TipOutDrawer />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <ViewArea />
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <BottomBar />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
 
-export default AppMain;
+export default withRouter(AppMain);

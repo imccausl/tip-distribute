8f003296f2eb9facfb970f50110a40e5e4f4f97c
Ian McCausland <imccausl@gmail.com>
2017-08-22 21:20:58 -0400

    Update helpers for some changes in data structure

--- src/helpers/populateStateHelpers.js
+++ src/helpers/populateStateHelpers.js
@@ -1,41 +1,89 @@
 function combinePeopleAndUsers(people, users) {
-  const combinedObj = people.concat(users);
-  return combinedObj;
+  return {
+    ...people,
+    ...users,
+  };
 }
 
-function matchPeopleToTipOuts(tipOut, allPeople) {
-  const tipOutState = Object.assign({}, tipOut);
-
-  tipOutState.people = tipOutState.people.map(person => [{
-    belongsTo: person.belongsTo,
-    id: person.person,
-    name: allPeople[person.person].displayName,
-    hours: person.hours,
-  }]);
-
-  return tipOutState;
+function mapObj(obj, callback) {
+  return Object.keys(obj).map(key => callback(key));
 }
+
+function matchPeopleToTipOuts(tipOut, allPeople, stores) {
+  const newTipOutState = Object.assign(tipOut);
+
+  newTipOutState.people = tipOut.people.map((person) => {
+    return {
+      belongsTo: person.belongsTo,
+      id: person.id,
+      name: allPeople[person.id].displayName,
+      hours: person.hours,
+    };
+  });
+
+  return newTipOutState;
+}
+
+function matchStoreToTipOuts(tipOut, stores) {
+  const newTipOutState = Object.assign(tipOut);
+
+  newTipOutState.store = stores[tipOut.store];
+
+  return newTipOutState;
+}
+
+function calculateTotalHours(tipOut) {
+  const newTipOutState = Object.assign(tipOut);
+  const strippedHours = tipOut.people.map(person => parseFloat(person.hours));
+  const totalHours = Math.round(strippedHours.reduce((curr, prev) => curr + prev, 0));
+
+  newTipOutState.totalHours = totalHours;
+
+  return newTipOutState;
+}
+
+function calculateWage(tipOut) {
+  let newTipOutState = Object.assign(tipOut);
+  let wage = 0;
+  let totalHours = tipOut.totalHours;
+  let totalCash = tipOut.totalCash
+
+  if (tipOut.people && tipOut.totalHours === 0) {
+    newTipOutState = calculateTotalHours(tipOut);
+    totalHours = newTipOutState.totalHours;
+  }
+
+  if (typeof totalHours !== 'number') {
+    totalHours = parseFloat(totalHours);
+  }
+
+  wage = parseFloat(tipOut.totalCash) / parseFloat(newTipOutState.totalHours);
+  newTipOutState.hourlyWage = wage;
+
+  return newTipOutState;
+}
+
 
 function getTipOutsCreatedByUser(profile, tipOuts) {
-  console.log("getTipOuts",profile);
-  return profile.tipOuts.created.map(key => tipOuts[key]);
+  return profile.tipOutsCreated.map(key => tipOuts[key]);
 }
 
-function getTipsBelongingToUser(tipOuts, users, id) {
-  return Object.keys(tipOuts).map((key, index) => {
-    if (users[id].tipOuts.belongsTo[index].id === key) {
-      const usersTips = Object.assign({}, tipOuts[key]);
-      usersTips.people = usersTips.people.filter(person => person.id === id);
+function getTipsBelongingToUser(profile, id, tipOuts) {
+  return profile.belongsTo.map((key) => {    
+    const newTipOutState = Object.assign(tipOuts[key]);
+    newTipOutState.people = tipOuts[key].people.filter(person => person.id === id);
 
-      return usersTips;
-    }
-    return null;
+    return newTipOutState;
   });
 }
 
 export {
   combinePeopleAndUsers,
   matchPeopleToTipOuts,
+  matchStoreToTipOuts,
   getTipOutsCreatedByUser,
   getTipsBelongingToUser,
+  calculateTotalHours,
+  calculateWage,
+  mapObj,
 };

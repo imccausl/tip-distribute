0a328a608ee1f334ddabc06884428bbcca7b12e2
Ian McCausland <imccausl@gmail.com>
2017-08-26 11:37:41 -0400

    Fix stores not passed to init function for individual tips

--- src/reducers/TipsBelongingToUser.js
+++ src/reducers/TipsBelongingToUser.js
@@ -3,7 +3,13 @@ import initializeTipsState from '../helpers/populateStateHelpers';
 export default function tipsBelongingToUser(state = {}, action) {
   switch(action.type) {
     case 'TIPS_BELONGING_TO':
-      return initializeTipsState(action.payload.profile, action.payload.tipOuts, null, null, action.type);
+      return initializeTipsState(
+        action.payload.profile,
+        action.payload.tipOuts,
+        null,
+        action.payload.stores,
+        action.type,
+      );
     default:
       return state;
   }

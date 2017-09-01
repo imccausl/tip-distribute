506d2fd01e5378f001a197833ace9766ba09468a
Ian McCausland <imccausl@gmail.com>
2017-09-01 18:46:10 -0400

    Create getPeopleFromStore and filterUsersAddedToTipOut helper functions

--- src/helpers/currentTipOutHelpers.js
+++ src/helpers/currentTipOutHelpers.js
@@ -6,6 +6,21 @@ export function getIndexOfPerson(list, name) {
   return list.map(person => person.name.toLowerCase()).indexOf(name.toLowerCase());
 }
 
+export function getPeopleFromStore(storeNum, stores, people) {
+  return stores[storeNum].people.map((id) => {
+    return {
+      id,
+      name: people[id].displayName,
+    };
+  });
+}
+
 export function sortByLastName(tipOut) {
   
+}
+
+export function filterUsersAddedToTipOut(allUsers, usersAdded) {
+  const compareTo = usersAdded.map(user => user.id);
+
+  return allUsers.filter(user => compareTo.indexOf(user.id) === -1);
 }

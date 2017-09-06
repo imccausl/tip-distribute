547ebb33fe2ee7681378f2c12ff8b2f17c829b69
Ian McCausland <imccausl@gmail.com>
2017-09-06 08:07:57 -0400

    View area margin adapts to selected view

--- src/components/ViewArea.jsx
+++ src/components/ViewArea.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeList from './EmployeeList.jsx';
 import UserProfile from './UserProfile.jsx';
+import DistributeTips from './DistributionReport.jsx';
 
 class ViewArea extends Component {
   render() {
+    const adjustMargin = (view) => {
+      const viewStyle = {
+        margin: '60px 0',
+        zIndex: '0',
+      };
+
+      if (view === 0) {
+        viewStyle.margin = '110px 0';
+      }
+
+      return viewStyle;
+    }
+
     const viewArea = () => {
       switch (this.props.view.type) {
         case 'SHOW_EDIT_VIEW':
@@ -12,12 +26,14 @@ class ViewArea extends Component {
         case 'SHOW_USER_PROFILE':
           return <UserProfile />;
         case 'SHOW_DISTRIBUTE_TIPS':
+          return <DistributeTips />
         default:
           return null;
       }
-    }
+    } 
+
     return (
-      <div style={{ margin: '110px 0', zIndex: '0' }}>
+      <div style={adjustMargin(this.props.view.payload)}>
         {viewArea()}
       </div>
     );

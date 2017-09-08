34ce110cb287f4f8306d3ada0bb2cf298966a24f
Ian McCausland <imccausl@gmail.com>
2017-09-07 21:50:49 -0400

    Connect store person compoent to store list view

--- src/components/EditStorePeople.jsx
+++ src/components/EditStorePeople.jsx
@@ -7,16 +7,18 @@ import IconMenu from 'material-ui/IconMenu';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/arrow-drop-down';
 import SvgIcon from 'material-ui/SvgIcon';
 import ContentAdd from 'material-ui/svg-icons/social/person-add';
+import StorePerson from './StorePerson.jsx';
 
 function mapStateToProps(state) {
   return {
     people: state.firebase.data.people,
     profile: state.firebase.profile,
     stores: state.firebase.data.stores,
+    users: state.firebase.data.users,
   };
 }
 
-@firebaseConnect(['/stores', '/people'])
+@firebaseConnect(['/stores', '/people', '/users'])
 @connect(mapStateToProps)
 export default class EditStorePeople extends Component {
   constructor(props) {
@@ -24,35 +26,65 @@ export default class EditStorePeople extends Component {
 
     const userPeopleRecord = this.props.people[this.props.profile.ref];
     const userStore = this.props.stores[userPeopleRecord.storeRef];
-
+    
     this.state = { userStore };
   }
 
   render() {
-    return (
-      <div style={{ position: 'fixed', zIndex: '5', width: '100%', top: '60px', left: '0' }}>
-        <Toolbar>
-          <ToolbarGroup>
-            <ToolbarTitle text={`Edit People at Store ${this.state.userStore.storeNum}`} />
-          </ToolbarGroup>
-          <ToolbarSeparator />
-          <ToolbarGroup>
-            <IconButton
-              tooltip="Add person to store"
-              onTouchTap={() => {
+    const renderPeople = () => {
+      console.log("Store people:", this.state.userStore);
+      return this.state.userStore.people.map((person) => {
+        let personEmail = null;
+        const personRecord = this.props.people[person];
 
-              }}
-            >
-              <SvgIcon><ContentAdd /></SvgIcon>
-            </IconButton>
-            <IconMenu
-              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-            >
-            </IconMenu>
-          </ToolbarGroup>
-        </Toolbar>
+        console.log("PersonRecord:", personRecord);
+
+        if (personRecord.userRef) {
+          personEmail = this.props.users[personRecord.userRef].email;
+        }
+
+        return (
+          <StorePerson
+            key={person}
+            name={personRecord.displayName}
+            partnerNum={personRecord.partnerNum}
+            storeRef={personRecord.storeRef}
+            email={personEmail}
+          />
+        );
+      });
+    };
+
+    return (
+      <div>
+        <div style={{ position: 'fixed', zIndex: '5', width: '100%', top: '60px', left: '0' }}>
+          <Toolbar>
+            <ToolbarGroup>
+              <ToolbarTitle text={`People at Store ${this.state.userStore.storeNum}`} />
+            </ToolbarGroup>
+            <ToolbarSeparator />
+            <ToolbarGroup>
+              <IconButton
+                tooltip="Add person to store"
+                onTouchTap={() => {
+
+                }}
+              >
+                <SvgIcon><ContentAdd /></SvgIcon>
+              </IconButton>
+              <IconMenu
+                iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+              >
+              </IconMenu>
+            </ToolbarGroup>
+          </Toolbar>
+        </div>
+
+        <div style={{ margin: '70px 0' }}>
+          {renderPeople()}
+        </div>
       </div>
     );
   }

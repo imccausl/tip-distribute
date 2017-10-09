6f433a4fb52cec36c30d15b1a31e89436caed441
Ian McCausland <imccausl@gmail.com>
2017-10-08 20:37:17 -0400

    Make test pass no matter how many days of passed since the dateCreated

--- tests/utils/calculateExpiry.test.js
+++ tests/utils/calculateExpiry.test.js
@@ -3,11 +3,11 @@ import { calculateExpiry } from '../../src/helpers/currentTipOutHelpers';
 
 const expect = chai.expect;
 const dateCreated = new Date('OCTOBER 1 2017');
-console.log(dateCreated);
+const diff = Math.floor((Date.now() - Date.parse(dateCreated)) / 1000 / 60 / 60 / 24);
 
 describe('calculateExpiry', () => {
   it('should return number of days until tip out is expired', () => {
     const daysToExpire = calculateExpiry(dateCreated, 90);
-    expect(daysToExpire).to.equal('83');
+    expect(daysToExpire).to.equal(90 - diff);
   });
 });

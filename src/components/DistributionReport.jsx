d607cb02d75b5727b6b2e58900f1115ecc6d9fd6
Ian McCausland <imccausl@gmail.com>
2017-10-11 10:42:29 -0400

    Add chip elements for status indication

--- src/components/DistributionReport.jsx
+++ src/components/DistributionReport.jsx
@@ -11,6 +11,8 @@ import Divider from 'material-ui/Divider';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
 import Avatar from 'material-ui/Avatar';
+import TextField from 'material-ui/TextField';
+import Chip from 'material-ui/Chip';
 import GenericAvatar from 'material-ui/svg-icons/action/face';
 import { hideModal } from '../actions/modalActions';
 import { calculateWage } from '../helpers/populateStateHelpers';
@@ -41,6 +43,10 @@ class Distribution extends Component {
     if (!this.state.people) {
       return null;
     }
+
+    const tipChipStyle = {
+      margin: '5px',
+    };
 
     const getTipOut = hours => {
       if (!hours || !this.state.people) {
@@ -85,6 +91,7 @@ class Distribution extends Component {
                 <GenericAvatar />
               </Avatar>
             }
+            showExpandableButton
             actAsExpander
           >
             <div className="bar" style={barStyle}>
@@ -92,7 +99,19 @@ class Distribution extends Component {
             </div>
           </CardHeader>
           <Divider />
-          <CardText expandable />
+          <CardText expandable>
+            <TextField
+              multiLine
+              floatingLabelText="Message"
+              style={{ padding: '0 5px', width: '90%' }}
+              rows={3}
+            />
+          </CardText>
+          <Divider />
+          <CardText expandable>
+            <Chip style={tipChipStyle}>Tip Out Expires in XX Days</Chip>
+            <Chip stype={tipChipStyle}>Tip Out Status</Chip>
+          </CardText>
         </Card>
       );
     });
@@ -118,12 +137,12 @@ class Distribution extends Component {
         <Toolbar style={{ position: 'fixed', left: '0', top: '60px', width: '100%', zIndex: '5' }}>
           <ToolbarGroup firstChild>
             <div style={{ fontFamily: 'Roboto', paddingLeft: '25px' }}>
-              <div style={{ fontSize: '16px', fontWeight: 'bold' }}>
+              <div style={{ fontSize: '16px', fontWeight: '500' }}>
                 {`Week Ending ${parseDate(this.props.viewModel.weekEnding)}`}
               </div>
               <div style={{ fontSize: '14px', fontWeight: 'normal' }}>
                 {`$${this.props.viewModel.totalCash} | ${this.props.viewModel
-                  .totalHours} hours | ${parseFloat(this.props.viewModel.hourlyWage).toFixed(
+                  .totalHours} hours | $${parseFloat(this.props.viewModel.hourlyWage).toFixed(
                   2,
                 )}/hour`}
               </div>

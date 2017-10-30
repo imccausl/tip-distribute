4d832c4be8f3d519a1395e415a46e3ef3cc2f945
Ian McCausland <imccausl@gmail.com>
2017-10-30 10:43:19 -0400

    Add file and basic plan for DisplayUserTipOut componenet

---
+++ src/components/DisplayUserTipOut.jsx
@@ -0,0 +1,13 @@
+/* DisplayUserTipOut.jsx
+ * 
+ * The component responsible for displaying tip outs to individual users. Information
+ * in this view includes: 
+ *     tip out amount, 
+ *     how much hourly, 
+ *     how much money total for the week,
+ *     some basic stats about the tip out (average, how it compares to previous weeks, etc.),
+ *     tip out message (if a message was attached),
+ *     when the tip out was completed,
+ *     how many days until it expires,
+ *     finally a button/checkmark to indicate whether tip out was picked up.
+ */

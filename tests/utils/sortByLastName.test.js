4f30e1d583ceea124839099616dfe19ea6a26fe9
Ian McCausland <imccausl@gmail.com>
2017-11-13 03:58:15 -0500

    Create test for sorting by last name

---
+++ tests/utils/sortByLastName.test.js
@@ -0,0 +1,96 @@
+import chai from 'chai';
+import { sortByLastName } from '../../src/helpers/currentTipOutHelpers';
+
+const expect = chai.expect;
+
+describe("sortByLastName(takes an array of people's names)", () => {
+  it("should locate the last name from a string containing the person's full name", () => {
+    const people = {
+      z1p: {
+        name: 'Jerry Paul Neusserman',
+        belongsTo: '1z',
+        id: '2u',
+        hours: '31.6',
+      },
+      z2p: {
+        name: 'Zeynab Zimmerman',
+        belongsTo: '1z',
+        id: '3u',
+        hours: '39.47',
+      },
+      z3p: {
+        name: 'Ian McCausland',
+
+        belongsTo: '1z',
+        id: '4u',
+        hours: '14.25',
+      },
+      z4p: {
+        name: 'John Price',
+        belongsTo: '1z',
+        id: '5u',
+        hours: '38.97',
+      },
+      z5p: {
+        name: 'Glenn Alderson',
+        belongsTo: '1z',
+        id: '6u',
+        hours: '19.58',
+      },
+      z6p: {
+        name: 'Chang soek Lee',
+        belongsTo: '1z',
+        id: '7u',
+        hours: '37.19',
+      },
+    };
+
+    const sorted = {
+      z5p: {
+        name: 'Glenn Alderson',
+        belongsTo: '1z',
+        id: '6u',
+        hours: '19.58',
+      },
+      z6p: {
+        name: 'Chang soek Lee',
+        belongsTo: '1z',
+        id: '7u',
+        hours: '37.19',
+      },
+      z3p: {
+        name: 'Ian McCausland',
+        belongsTo: '1z',
+        id: '4u',
+        hours: '14.25',
+      },
+      z1p: {
+        name: 'Jerry Paul Neusserman',
+        belongsTo: '1z',
+        id: '2u',
+        hours: '31.6',
+      },
+      z4p: {
+        name: 'John Price',
+        belongsTo: '1z',
+        id: '5u',
+        hours: '38.97',
+      },
+      z2p: {
+        name: 'Zeynab Zimmerman',
+        belongsTo: '1z',
+        id: '3u',
+        hours: '39.47',
+      },
+    };
+
+    const sortedPeople = sortByLastName(people);
+    expect(sortedPeople).to.deep.equal(sorted);
+  });
+
+  it("should find the last name even with names ending in 'Sr.', 'Jr.' or 'I', 'II', etc.", () => {});
+
+  it("should put 'phantoms' (i.e. replacement/substitute workers) at the bottom of the list, ordering by employee number", () => {});
+
+  it('should return an intelligable error if an object is not passed to it', () => {});
+});

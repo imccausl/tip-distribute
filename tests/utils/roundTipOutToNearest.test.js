c06aceb71582404c2c4676859021df5d05ce4c25
Ian McCausland <imccausl@gmail.com>
2017-11-27 10:11:49 -0500

    Fix test returning string instead of float

--- tests/utils/roundTipOutToNearest.test.js
+++ tests/utils/roundTipOutToNearest.test.js
@@ -7,10 +7,10 @@ describe('roundTipOutToNearest', () => {
   it('should return wage given # of hours and total money, rounded to nearest value', () => {
     const wage = 0.62;
     const hours = 24;
-    const quarter = roundTipOutToNearest(wage, hours, 'QUARTER');
-    const dollar = roundTipOutToNearest(wage, hours, 'DOLLAR');
-    const nickel = roundTipOutToNearest(wage, hours, 'NICKEL');
-    const dime = roundTipOutToNearest(wage, hours, 'DIME');
+    const quarter = parseFloat(roundTipOutToNearest(wage, hours, 'QUARTER'));
+    const dollar = parseFloat(roundTipOutToNearest(wage, hours, 'DOLLAR'));
+    const nickel = parseFloat(roundTipOutToNearest(wage, hours, 'NICKEL'));
+    const dime = parseFloat(roundTipOutToNearest(wage, hours, 'DIME'));
 
     expect(quarter).to.equal(14.75);
     expect(dollar).to.equal(14);
